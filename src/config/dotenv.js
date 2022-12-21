14b977d361390e3f713b1a7e37ab4fc202ab2b46
Bryan Solares <solares.josue@outlook.com>
2022-12-20 19:31:11 -0600

    feat: Configuracion conexión con base de datos
    Se establece comunicación con base de datos con éxito.
    Se ha elegido Sequelize com ORM para conexión y manipulación de datos a
    POSTGRESQL. Se establecen configuraciones iniciales a partir de
    variables de entorno y se configura un pool de conexiónes por defecto de
    min: 5 a max: 100.
    
    Se analizará trabar con modelos. Se tiene que profundizar en lectura de
    la documentación.
    
    Review-by: Bryan Solares

--- src/config/dotenv.js
+++ src/config/dotenv.js
@@ -8,5 +8,10 @@ module.exports = {
   MONGO_DATABASE_NAME: process.env.DATABASE_NAME,
   ENV: process.env.NODE_ENV,
   TOKEN_EXPIRATION: process.env.JWT_EXPIRATION,
-  SECRET_KEY: process.env.JWT_SECRET_KEY
+  SECRET_KEY: process.env.JWT_SECRET_KEY,
+  POSTGRES_USER: process.env.POSTGRES_USER,
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+  POSTGRES_DB: process.env.POSTGRES_DB,
+  POSTGRES_HOST: process.env.POSTGRES_HOST,
+  POSTGRES_PORT: process.env.POSTGRES_PORT
 }
